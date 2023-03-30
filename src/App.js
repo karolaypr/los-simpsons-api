040972a32acfb863c51bc294f5f3b88c35947895
karolaypr <karopr8@gmail.com>
2023-03-30 17:33:26 -0500

    feat: update characters component name

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import './App.css';
 import simpsons from './img/simpsons.png';
 import { useState } from 'react';
-import Character from './components/characters';
+import Characters from './components/Characters';
 
 function App() {
   const [characters, setCharacters] = useState(null);
   const getCharacters = async () => {
     try {
+      const apiCharacters = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=50');
+      const jsonCharacters = await apiCharacters.json();
+    
+      let characterMap = jsonCharacters.map(item=>{
+        return [item.character,item]
+      });
+      let characterMapArr = new Map(characterMap); // Pares de clave y valor
+      let uniqueCharacters = [...characterMapArr.values()]; // Conversión a un array
+      console.log(uniqueCharacters);
+      
+      uniqueCharacters.sort((x, y) => x.character.localeCompare(y.character)); //Ordenar de manera ascendente 
 
+      setCharacters(uniqueCharacters);
     } catch (error) {
       console.log(error);
     }
-    const apiCharacters = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=50');
-    const jsonCharacters = await apiCharacters.json();
-    
-    let characterMap = jsonCharacters.map(item=>{
-      return [item.character,item]
-    });
-    let characterMapArr = new Map(characterMap); // Pares de clave y valor
-    let uniqueCharacters = [...characterMapArr.values()]; // Conversión a un array
-    console.log(uniqueCharacters);
-    
-    uniqueCharacters.sort((x, y) => x.character.localeCompare(y.character)); //Ordenar de manera ascendente 
-
-    setCharacters(uniqueCharacters);
   }
   
   return (
@@ -31,7 +30,7 @@ function App() {
       <div className="App-header">
         <h1 className="title">Los Simpsons</h1>
         {characters ? (
-          <Character characters={characters} setCharacters={setCharacters} />
+          <Characters characters={characters} setCharacters={setCharacters} />
         ) : (
           <>
             <img src={simpsons} alt="Los Simpsons" className="img-home" />
